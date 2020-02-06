876bdb6ce3bca838c2deccc29e6db3e53a9969d7
Tyler Moore <tmoorewebdesigns@gmail.com>
2020-02-06 01:34:32 -0500

    Update PageWeddings.vue

--- src/pages/PageWeddings.vue
+++ src/pages/PageWeddings.vue
@@ -1,81 +1,76 @@
 <template>
   <q-page padding>
-		<q-btn
-			to="/"
-			:style="
+    <q-btn
+      to="/"
+      :style="
       $q.dark.isActive
         ? 'color: white'
         : 'color: #1e1e1e'"
-			icon="chevron_left"
-			v-bind:label="$t('mainMenu.back')"
-			flat />
+      icon="chevron_left"
+      v-bind:label="$t('mainMenu.back')"
+      flat
+    />
 
-		<h5 
+    <h5
       style="font-weight: bold"
       :style="
           $q.dark.isActive
             ? 'color: white'
             : 'color: #1e1e1e'"
-      >{{ $t('mainMenu.weddings') }}
-      </h5>
-      
+    >{{ $t('mainMenu.weddings') }}</h5>
+
     <div class="space"></div>
-        <!-- Begin Mobile Section-->
-        <div class="column items-center">
+    <!-- Begin Mobile Section-->
+    <div class="column items-center">
+      <!-- Mobile Card 1 -->
 
-        <!-- Mobile Card 1 -->
-      
-        <oak-weddings-card></oak-weddings-card>
+      <oak-weddings-card></oak-weddings-card>
 
-        <!-- End Mobile Card 1 -->
+      <!-- End Mobile Card 1 -->
 
-        <div class="space" />
+      <div class="space" />
 
-        <!-- Begin Mobile Card 2 -->
-        <kennedya-weddings-card></kennedya-weddings-card>
-        <!-- End Mobile Card 2 -->
+      <!-- Begin Mobile Card 2 -->
+      <kennedya-weddings-card></kennedya-weddings-card>
+      <!-- End Mobile Card 2 -->
 
-        <div class="space" />
+      <div class="space" />
 
-        <!-- Begin Mobile Card 3 -->
-        <atrium-card></atrium-card>
-        <!-- End Mobile Card 3 -->
-
-        </div>
-        <!-- End Mobile Section -->
+      <!-- Begin Mobile Card 3 -->
+      <atrium-card></atrium-card>
+      <!-- End Mobile Card 3 -->
+    </div>
+    <!-- End Mobile Section -->
 
     <footer />
-
-    </q-page>   
+  </q-page>
 </template>
 
 <script>
-import { openURL } from 'quasar'
-import OakWeddingsCard from "components/cards/OakWeddingsCard.vue"
-import KennedyaWeddingsCard from "components/cards/KennedyaWeddingsCard.vue"
-import AtriumCard from "components/cards/AtriumCard.vue"
-import Footer from 'components/footer/Footer.vue'
-    
-	export default {
-    components: {
-      OakWeddingsCard,
-      KennedyaWeddingsCard,
-      AtriumCard,
-      Footer
-    },
-    data() {
-      return {  
-        slide: 1,
-        tab: 'one',
-        splitterModel: 15,
-      }
-    }
-  } 
+import { openURL } from "quasar";
+import OakWeddingsCard from "components/cards/OakWeddingsCard.vue";
+import KennedyaWeddingsCard from "components/cards/KennedyaWeddingsCard.vue";
+import AtriumCard from "components/cards/AtriumCard.vue";
+import Footer from "components/footer/Footer.vue";
 
+export default {
+  components: {
+    OakWeddingsCard,
+    KennedyaWeddingsCard,
+    AtriumCard,
+    Footer
+  },
+  data() {
+    return {
+      slide: 1,
+      tab: "one",
+      splitterModel: 15
+    };
+  }
+};
 </script>
 <style lang="scss" scoped>
-  .space {
-    padding: 2rem;
-  }
-
+.space {
+  padding: 2rem;
+}
 </style>
